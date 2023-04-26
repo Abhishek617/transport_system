8c54537d263f90250b5491a46a242a2f4551f406
NitiDial2web <niti.dail2web@gmail.com>
2023-04-26 15:04:38 +0530

    common widgets

---
+++ lib/utils/appColors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const kWhite = Colors.white;
+  static const kBlack = Colors.black;
+  static const kBlue = Color(0xFF407BFF);
+  static const kLightBlue = Color(0xFF87ACFF);
+  static const kGreen = Color(0xFF0DEC4B);
+}
