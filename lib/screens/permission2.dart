2811e9da9074288a4d5ee67cdec9632fccc2b5c8
Abhishek singh <abhishek@phpdots.com>
2023-04-26 16:30:30 +0530

    All Done

---
+++ lib/screens/permission2.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Location_Icon extends StatefulWidget {
+  const Location_Icon({Key? key}) : super(key: key);
+
+  @override
+  State<Location_Icon> createState() => _Location_IconState();
+}
+
+class _Location_IconState extends State<Location_Icon> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+
+          )
+        ],
+      ),
+    );
+  }
+}
